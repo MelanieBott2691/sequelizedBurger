20282c7568a5172424ee5bc6fc73c5d7723da7f0
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-05 16:59:54 -0600

    set up burgers_controller.js file to get burger, udpate and create

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,58 +1,33 @@
 // router connections that will manage the database arrays and identify the endpoints
 var express = require("express");
-
 var router = express.Router();
-
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Router Index
+// Router Index Get Burger
 router.get("/", function(req, res) {
   // res.redirect('/burgers'); 
-  burger.selectAll(function(data) {
+  burger.Burger.findAll({ include: [{ model: db.customer }], order: [ [ 'Name' ]]}).then(function(data) {
     var hdbrsObj = {
       burgers: data
     };
     console.log(hdbrsObj);
     res.render("index", hdbrsObj);
   });
-
-  router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-   ], function(result) {
-      // console.log("Please Enter Burger Name!");
-      res.json({ id: result.insertId });
-   });
-  });
-
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-    burger.updateOne({ devoured: req.body.devoured }, condition, function (
-      result
-    ) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      }else{
-        res.status(200).end();
-      }
-    });
-  });
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  console.log("condition", condition);
-
-  burger.deleteOne(condition, function(result) {
-    if (result.changedRows === 0) {
-      return res.status(404).end();
-    }else {
-      res.status(200).end();
-    }
+});
+// Create Burger
+router.post("/burgers/create", function(req, res) {
+  burger.Burger.create({Name: req.body.burger_name}).then(function(burger_db){
+    res.redirect('/burgers');
   });
 });
+// Update Burger
+router.put('/burgers/update/:burger_name', function(req, res){
+  burger.Customer.findOrCreate({where: {Name: req.body.customer_name}}).spread((customer) => {
+    customer.get({ plain: true});
+    db.burger.update({devoured: 1, CustomerId: customer.id}, {where: {id: req.params.burger_name}}).then(function(burger_db) {
+      res.redirect('/burgers');
+    })
+  })
 });
 module.exports = router;
