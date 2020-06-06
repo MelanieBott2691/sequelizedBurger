aa5f62860d04196d6a79544d124a6ad417496a54
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-05 23:09:07 -0600

    set up DataTypes in burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,22 @@
 
 module.exports = function(sequelize, DataTypes) {
-  var burger = sequelize.define('burger', {
-      text: DataTypes.STRING,
-      complete: DataTypes.BOOLEAN
+  var Burger = sequelize.define('Burger', {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    paranoid: true,
+    tableName: "burgers",
+    classMethods: {
+      associate: function(models) {
+        Burger.belongsTo(models.Customer);
+      }
+    }
   });
   return burger
   };
