3aa85afbdb427b69f156f111438f2f2ee296030f
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-05 23:37:40 -0600

    deleted files and folders that are not needed that I set up following sequelize manual

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
--- USE burgers_db;
--- INSERT INTO burgers(burger_name, devoured) 
--- VALUES('Hawaiian BBQ Burger', false), ('Little Bacon Cheeseburger', false), 
--- ('Scrum-Delicious Burger', false), ('Chile Veggie Burger', false);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian BBQ Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Little Bacon Cheeseburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Scrum-Delicious Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chile Veggie Burger', FALSE);
