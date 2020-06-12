dc2923ed6a375ce1c8b9ee18c8c50a4e4377fd24
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-12 02:18:37 -0600

    changed host to localhost instead of 127.0.0.1 and changed server calling main to index and now there are half the errors. Still says access denied for user

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(methodOverride('_method'));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'index' }));
 app.set('view engine', 'handlebars');
 
 var router = require('./controllers/burgers_controllers.js');
