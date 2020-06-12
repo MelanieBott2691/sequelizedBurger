9cf168174cc1a5f8a5f1df1c0ac810a82dbaef58
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-12 02:11:17 -0600

    App listening on port 8081 but error says that Unhandled rejection SequelizeAccessDeniedError: Access denied for user 'root'@'localhost' (using password: YES)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
 // Set up the Express App
